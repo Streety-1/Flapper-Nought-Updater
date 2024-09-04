ebb66b45d3bff3ee801673fbea3f8a5b1b3abf3a
Streety <88004408+Streety-1@users.noreply.github.com>
2024-09-04 18:38:04 +0100

    Add files via upload

---
+++ FlapperUpdater.py
@@ -0,0 +1,72 @@
+# Download Latest version from github repo
+# https://github.com/Streety-1/Flapper-Nought.git
+
+#run updater to launch script 
+
+import os
+import subprocess
+import sys
+import urllib.request
+import time
+import shutil
+
+gitRepo = 'https://github.com/Streety-1/Flapper.git'
+
+urltocheckwifi = "https://www.google.com/ "
+
+requiredmodules = {'simple-term-menu'}
+
+Flapper_File_Location = os.getcwd() + r"\Flapper" #drive\Flapper
+
+#-------------------Prequisit Installer-------------------#
+
+def systemCmd(command):
+    os.system(command)
+
+def pipinstall(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package], stdout=subprocess.DEVNULL)
+
+try:
+    pipinstall('requests')
+    import requests
+    urllib.request.urlopen(urltocheckwifi)
+
+    print('''
+    ╔═════════════════════════════════════════════╗   
+      Getting stuff ready....      
+    ╚═════════════════════════════════════════════╝
+    
+    ''')
+
+    systemCmd('python.exe -m pip install --upgrade pip --quiet')
+
+    #install modules
+    print("         modules")
+    for x in requiredmodules:
+        try:
+            import x
+            print("\033[2J\033[H", end="", flush=false)
+        except ImportError:
+            pipinstall(x)
+
+    print("         dbus")
+    systemCmd('sudo apt install python-dbus --quiet')
+
+    print("         git")
+    systemCmd('sudo apt install git --quiet')
+
+    print("         updating scripts")
+    #Delete existing Flapper Folder
+    systemCmd('sudo rm -rf '+Flapper_File_Location)
+
+    #Install new repo file
+    systemCmd('git clone '+gitRepo)
+
+    #Run main.py in new repo
+    exec(open(Flapper_File_Location+r'\main.py').read())
+
+except urllib.error.URLError:
+    print("!!! Error: No wifi connection, cannot get latest version !!!")
+    time.sleep(3)
+    exec(open(Flapper_File_Location + r'\main.py').read())
+
